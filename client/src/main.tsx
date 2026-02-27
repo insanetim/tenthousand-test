450b65e5c9bc86aca277284891e3924e13809b50
insanetim <47120724+insanetim@users.noreply.github.com>
2026-02-27 13:39:54 +0200

    setup base api provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,18 +1,22 @@
+import { ApiProvider } from "@reduxjs/toolkit/query/react"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { Route, BrowserRouter as Router, Routes } from "react-router"
+import { api } from "./api/baseApi.ts"
 import App from "./App.tsx"
 import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route
-          element={<App />}
-          path="/*"
-        />
-      </Routes>
-    </Router>
+    <ApiProvider api={api}>
+      <Router>
+        <Routes>
+          <Route
+            element={<App />}
+            path="/*"
+          />
+        </Routes>
+      </Router>
+    </ApiProvider>
   </StrictMode>
 )
